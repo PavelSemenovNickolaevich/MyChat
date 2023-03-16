619298d921a8a615d4254ea925f3e802eed3f133
Pavel Semenov <PNSemenov@sberbank.ru>
2023-03-16 08:42:35 +0300

    added integration with firebase, google auth, chat and signin activity

---
+++ app/src/main/java/ru/android/mychat/MainActivity.kt
@@ -0,0 +1,46 @@
+package ru.android.mychat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import ru.android.mychat.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val database = Firebase.database
+        val myRef = database.getReference("message")
+
+        binding.bSend.setOnClickListener {
+            myRef.setValue(binding.adMessage.text.toString())
+        }
+        onChangeListener(myRef)
+    }
+
+    private fun onChangeListener(dRef: DatabaseReference) {
+        dRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                binding.apply {
+                    rcView.append("\n")
+                    rcView.append("Stalin: ${snapshot.value.toString()}")
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        })
+    }
+}
